669199f2b0d43b243d83b79ec9b7a5e7bf2d794c
oh29oh29 <oh29oh29.develop@gmail.com>
2021-02-13 19:24:58 +0900

    ExceptionHandler

---
+++ exception-handler/README.md
@@ -0,0 +1,6 @@
+# @ExceptionHandler
+
+컨트롤러 레벨에서 처리하는 방법.  
+
+단점
+> @ExceptionHandler 주석이 달린 메서드는 해당 특정 컨트롤러에 대해서만 유효하다.
---
+++ exception-handler/src/main/java/com/oh29oh29/exceptionhandler/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.oh29oh29.exceptionhandler.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        throw new RuntimeException();
+    }
+
+    @ExceptionHandler({ RuntimeException.class })
+    public String handleException() {
+        return "ExceptionHandler test";
+    }
+}
